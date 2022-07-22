904857745d78e255149bdd826d732db5a0c4de75
chottedifs <husadif.aldi@gmail.com>
2022-07-23 02:25:45 +0700

    Update Route API

--- app/Http/Controllers/API/InformationController.php
+++ app/Http/Controllers/API/InformationController.php
@@ -11,29 +11,6 @@ class InformationController extends Controller
     public function all(Request $request){
         $informations = Informasi::all();
 
-        // foreach ($tagihan as $dataTagihan){
-        //     $response[] = [
-        //         'id_sewa' => $dataTagihan->SewaKios->id,
-        //         'id_tagihan' => $dataTagihan->id,
-        //         'kode_tagihan' => $dataTagihan->kode_tagihan,
-        //         'nama_penyewa' => $dataTagihan->User->nama_lengkap,
-        //         'no_rekening' => $dataTagihan->User->no_rekening,
-        //         'nama_kios' => $dataTagihan->SewaKios->RelasiKios->Kios->nama_kios,
-        //         'lokasi_kios' => $dataTagihan->SewaKios->RelasiKios->Lokasi->nama_lokasi,
-        //         'periode' => $dataTagihan->periode,
-        //         'tagihan_kios' => $dataTagihan->tagihan_kios,
-        //         'kwh' => $dataTagihan->total_kwh,
-        //         'tagihan_kwh' => $dataTagihan->tagihan_kwh,
-        //         'total_tagihan' => $dataTagihan->tagihan_kwh + $dataTagihan->tagihan_kios,
-        //         'status_bayar' => $dataTagihan->MasterStatus->nama_status
-        //         // 'nama_kios' => $dataKios->RelasiKios->Kios->nama_kios,
-        //         // 'tipe_kios' => $dataKios->RelasiKios->TarifKios->tipe,
-        //         // 'tarif_kios' => $dataKios->RelasiKios->TarifKios->harga,
-        //         // 'tempat_kios' => $dataKios->RelasiKios->Kios->tempat,
-        //         // 'lokasi_kios' => $dataKios->RelasiKios->Lokasi->nama_lokasi
-        //     ];
-        // };
-
         if($informations)
             return ResponseFormatter::success($informations, 'Data Sewa Kios Berhasil di Ambil');
         else
--- routes/api.php
+++ routes/api.php
@@ -27,21 +27,9 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('tagihan', [TagihanController::class, 'all']);
     Route::get('informations', [InformationController::class, 'all']);
     Route::post('logout', [AuthController::class, 'logout']);
-    // Route::get('kios', [KiosController::class, 'all']);
-    // Route::get('user', [SewaKiosController::class, 'all']);
-    // Route::get('tagihan', [SewaKiosController::class, 'all']);
-    // Route::get('pembayaran', [SewaKiosController::class, 'all']);
-
-
-    // Route::get('/home', [UserController::class, 'home']);
-    // Route::get('/tagihan', [UserController::class, 'tagihan']);
-    // Route::get('/detail-kios', [UserController::class, 'detailKios']);
-    // Route::get('/user-kios', [UserController::class, 'userKios']);
-    // Route::get('/detail-informasi/{id}', [UserController::class, 'detailInformasi']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
-
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
